59810e94a7c172f69dec8df8049f9c4688fcc678
Sudeep MP <sudipmelepunathilmp@gmail.com>
2024-07-21 17:48:51 +0000

    switched to vite tailwind compilation rather than the cdn link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 import KafkaDiagramAndChart from "./KafkaDiagramAndChart";
 
 const App = () => {
   return (
     <div className="p-4 bg-white">
+      <Button>Shadcn button</Button>
       <KafkaDiagramAndChart />
     </div>
   );
